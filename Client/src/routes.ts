564ec07c40fb85535f71510ab8d4a8f4222189db
weichweich <weich@posteo.de>
2017-03-21 11:06:54 +0100

    updated routes

--- Client/src/routes.ts
+++ Client/src/routes.ts
@@ -2,16 +2,15 @@
 
 export function routes(route) {
 	route('login', { page: 'login-page', stack: false })
-    route('/', { page: 'main-page', stack: false }, function() {
-        route('pins', { page: 'pins-fragment', stack: true }, function() {
-            route('add-pin', { path: '/pin/add', page: 'add-pin-fragment', stack: true })
-            route('sequences', { path: '/pins/:pinId', page: 'sequences-fragment', stack: true }, function() {
-                route('add-sequence', { path: '/pins/:pinId/add', page: 'add-sequence-fragment', stack: true })
-            })
+    route('/', { page: 'main-page', stack: false })
+    route('pins', { page: 'pins-fragment', stack: true }, function() {
+        route('add-pin', { path: 'add', page: 'add-pin-fragment', stack: true })
+        route('sequences', { path: 'pin/:pinId', page: 'sequences-fragment', stack: true }, function() {
+            route('add-sequence', { path: 'add', page: 'add-sequence-fragment', stack: true })
         })
-        route('users', { page: 'users-fragment', stack: true }, function() {
-            route('add-user', { path: '/users/add', page: 'add-user-fragment', stack: true })
-            route('user', { path: '/users/:userId', page: 'user-fragment', stack: true })
-        })
+    })
+    route('users', { page: 'users-fragment', stack: true }, function() {
+        route('add-user', { path: 'add', page: 'add-user-fragment', stack: true })
+        route('user', { path: 'user/:userId', page: 'user-fragment', stack: true })
     })
 }
